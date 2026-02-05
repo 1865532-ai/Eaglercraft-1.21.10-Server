c633db9aa9a65ef07e4f11843c5f0c80ae12c7d7
JmooALT <franciscocrk12@gmail.com>
2026-02-05 03:14:42 +0000

    working 1.21.10

--- startup.sh
+++ startup.sh
@@ -1,9 +1,17 @@
-echo "Starting limbo..."
-sleep 1
-cd limbo && java -jar server.jar &
-echo "Starting velocity..."
-sleep 1
-cd velocity && java -jar server.jar &
-echo "Starting server..."
-sleep 1
-cd server && java -jar server.jar
+#!/bin/bash
+
+echo "Starting Velocity proxy..."
+cd velocity
+java -jar velocity-3.5.0-all.jar &
+sleep 3  # wait a few seconds for port 25567 to open
+cd ..
+
+echo "Starting Limbo..."
+cd limbo
+java -jar server.jar &
+sleep 2
+cd ..
+
+echo "Starting Paper server..."
+cd server
+java -jar server.jar
